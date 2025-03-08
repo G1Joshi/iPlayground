cf26c1a661069644f471983b6066cd72fac2e593
Jeevan Joshi <joshijeevan98@gmail.com>
2025-03-08 10:27:07 +0530

    pattern 3

--- iPlayground.playground/Pages/Patterns.xcplaygroundpage/Contents.swift
+++ iPlayground.playground/Pages/Patterns.xcplaygroundpage/Contents.swift
@@ -25,3 +25,16 @@ func pattern2(n: Int) {
 run("Pattern 2") {
     pattern2(n: 5)
 }
+
+func pattern3(n: Int) {
+    for i in 1...n {
+        for j in 1...i {
+            print(j, terminator: " ")
+        }
+        print()
+    }
+}
+
+run("Pattern 3") {
+    pattern3(n: 5)
+}
